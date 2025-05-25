4b19c21ec694ff6a62b3f18e7f7a53910d8b8481
sanjayshrestha <sanjayshrestha772@gmail.com>
2025-05-25 14:57:55 +0545

    resolve dotenv and database connection issues

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,6 @@
-require('dotenv').config()
-const mongoose=require('mongoose');
+require("dotenv").config();
 
-const connectDB=async()=>{
-  await mongoose.connect(process.env.MONGO_URI)
-}
-
-
-
-
-module.exports=connectDB
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+};
+module.exports = connectDB;
